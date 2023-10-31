76830b18842c9ee4e5dd66b835fd58980ff17f28
lin.zl <leo.zl>
2023-10-31 20:31:46 +0800

    feat: remove user component

--- config/routes.ts
+++ config/routes.ts
@@ -11,17 +11,6 @@
  * @doc https://umijs.org/docs/guides/routes
  */
 export default [
-  // {
-  //   path: '/user',
-  //   layout: false,
-  //   routes: [
-  //     {
-  //       name: 'login',
-  //       path: '/user/login',
-  //       component: './User/Login',
-  //     },
-  //   ],
-  // },
   {
     path: '/welcome',
     name: 'welcome',
