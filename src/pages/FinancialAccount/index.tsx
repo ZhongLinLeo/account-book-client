7479a9092644db0ee116d1e7985e0d22c61d42bd
lin.zl <leo.zl>
2023-11-01 22:30:55 +0800

    feat: add account card list

--- src/pages/FinancialAccount/index.tsx
+++ src/pages/FinancialAccount/index.tsx
@@ -1,23 +1,17 @@
 import CreateClassifyForm from '@/pages/Classify/components/CreateClassifyForm';
-import { DeleteFilled, EditFilled, PlusOutlined } from '@ant-design/icons';
+import { DeleteFilled, EditFilled, EyeFilled, EyeInvisibleFilled } from '@ant-design/icons';
+import { ProList } from '@ant-design/pro-components';
 import { PageContainer } from '@ant-design/pro-layout';
-import type { ActionType, ProColumns } from '@ant-design/pro-table';
-import {
-  ProFormRadio,
-  ProFormSwitch,
-  ProList,
-} from '@ant-design/pro-components';
-import { Button, message, Card, List, Typography } from 'antd';
+import type { ActionType } from '@ant-design/pro-table';
+import { Typography, message } from 'antd';
 import React, { useRef, useState } from 'react';
+import { useRequest } from 'umi';
 import type { FormValueType } from './components/UpdateClassifyForm';
 import UpdateClassifyForm from './components/UpdateClassifyForm';
 import type { FinancialAccount } from './data.d';
-import { addAccount, accounts, removeAccount, updateAccount } from './service';
-import { useRequest } from 'umi';
-import styles from './style.less';
+import { accounts, addAccount, removeAccount, updateAccount } from './service';
 
 const { Paragraph } = Typography;
-
 
 /**
  * 添加节点
@@ -93,65 +87,73 @@ const FinancialAccountCard: React.FC = () => {
 
   const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
   const actionRef = useRef<ActionType>();
-  const [currentRow, setCurrentRow] = useState<FinancialAccount>();
+
+  const [currentCard, setCurrentCard] = useState<FinancialAccount>();
   /** 国际化配置 */
 
   const { data, loading } = useRequest(() => {
     return accounts();
   });
 
+  const [visible, setVisible] = useState(false);
+
   const list = data || [];
-  const nullData: Partial<FinancialAccount> = {};
+
+  const item = list.map((account) => ({
+    title: account.accountName,
+    description: account.accountDescribe,
+    actions: [
+      <EditFilled
+        onClick={() => {
+          setCurrentCard(account);
+          handleUpdateModalVisible(true);
+        }}
+        style={{ color: '#4E89FF' }}
+      />,
+      <DeleteFilled
+        onClick={async () => {
+          await handleRemove(account);
+          actionRef.current?.reloadAndRest?.();
+        }}
+        style={{ color: 'red' }}
+      />,
+      <EyeFilled
+        hidden={visible}
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      />,
+      <EyeInvisibleFilled
+        hidden={!visible}
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      />,
+    ],
+    content: 'dkfasjf',
+  }));
 
   return (
     <PageContainer>
-      <div className={styles.cardList}>
-        <List<Partial<FinancialAccount>>
-          rowKey='id'
-          loading={loading}
-          grid={{
-            gutter: 16,
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 4,
-            xxl: 4,
-          }}
-          dataSource={[nullData, ...list]}
-          renderItem={(item) => {
-            console.log(item);
-            if (item && item.accountId) {
-              return (
-                <List.Item key={item.accountId}>
-                  <Card
-                    hoverable
-                    className={styles.card}
-                    actions={[<a key='option1'>操作一</a>, <a key='option2'>操作二</a>]}
-                  >
-                    <Card.Meta
-                      // avatar={<img alt='' className={styles.cardAvatar} src={item.avatar} />}
-                      title={<a>{item.accountName}</a>}
-                      description={
-                        <Paragraph className={styles.item} ellipsis={{ rows: 3 }}>
-                          {item.accountDescribe}
-                        </Paragraph>
-                      }
-                    />
-                  </Card>
-                </List.Item>
-              );
-            }
-            return (
-              <List.Item>
-                <Button type="dashed" className={styles.newButton}>
-                  <PlusOutlined /> 新增账户
-                </Button>
-              </List.Item>
-            );
-          }}
-        />
-      </div>
+      <ProList<FinancialAccount>
+        ghost
+        itemCardProps={{
+          ghost: true,
+        }}
+        showActions="hover"
+        rowSelection={{}}
+        grid={{ gutter: 16, column: 2 }}
+        metas={{
+          title: {},
+          description: {},
+          type: {},
+          content: {},
+          actions: {
+            cardActionProps: 'actions',
+          },
+        }}
+        dataSource={item}
+      />
 
       <CreateClassifyForm
         onFinish={async (value) => {
@@ -168,10 +170,10 @@ const FinancialAccountCard: React.FC = () => {
       />
       <UpdateClassifyForm
         onFinish={async (value) => {
-          const success = await handleUpdate(value, currentRow);
+          const success = await handleUpdate(value, currentCard);
           if (success) {
             handleUpdateModalVisible(false);
-            setCurrentRow(undefined);
+            setCurrentCard(undefined);
             if (actionRef.current) {
               actionRef.current.reload();
             }
@@ -179,7 +181,7 @@ const FinancialAccountCard: React.FC = () => {
         }}
         onOpenChange={handleUpdateModalVisible}
         updateModalVisible={updateModalVisible}
-        value={currentRow || {}}
+        value={currentCard || {}}
       />
     </PageContainer>
   );
