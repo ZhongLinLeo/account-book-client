bbe5f0798c667e354f2e426353abbe70e1979c56
lin.zl <lin.zl@dtwave-inc.com>
2023-11-07 17:37:13 +0800

    feat(dashboard): add funds record template

---
+++ src/pages/Dashboard/components/Overview.tsx
@@ -0,0 +1,103 @@
+import { StatisticCard } from '@ant-design/pro-components';
+import React from 'react/index';
+import { FundsOverview } from '@/pages/Dashboard/data';
+// import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+// import { Card, Col, Row, Statistic } from 'antd';
+
+const { Statistic } = StatisticCard;
+const { Divider } = StatisticCard;
+
+export type OverviewProps = {
+  value: Partial<FundsOverview>;
+};
+
+const Overview: React.FC<OverviewProps> = (props) => {
+  return (
+    <StatisticCard.Group>
+      <StatisticCard title="总概览">
+        <Statistic
+          title="收入"
+          precision={2}
+          value={props.value.overview?.income}
+          layout={'vertical'}
+          status={'success'}
+          prefix={'¥'}
+          valueStyle={{ color: '#3f8600' }}
+        />
+        <Statistic
+          title="支出"
+          precision={2}
+          value={props.value.overview?.expenditure}
+          layout={'vertical'}
+          status="error"
+          prefix={'¥'}
+          valueStyle={{ color: '#cf1322' }}
+        />
+      </StatisticCard>
+      <Divider />
+      <StatisticCard title="年概览">
+        <Statistic
+          title="收入"
+          precision={2}
+          value={props.value.yearOverview?.income}
+          layout={'vertical'}
+          status={'success'}
+          prefix={'¥'}
+          valueStyle={{ color: '#3f8600' }}
+        />
+        <Statistic
+          title="支出"
+          precision={2}
+          value={props.value.yearOverview?.expenditure}
+          layout={'vertical'}
+          status="error"
+          prefix={'¥'}
+          valueStyle={{ color: '#cf1322' }}
+        />
+      </StatisticCard>
+      <Divider />
+      <StatisticCard title="月概览">
+        <Statistic
+          title="收入"
+          precision={2}
+          value={props.value.monthOverview?.income}
+          layout={'vertical'}
+          status={'success'}
+          prefix={'¥'}
+          valueStyle={{ color: '#3f8600' }}
+        />
+        <Statistic
+          title="支出"
+          precision={2}
+          value={props.value.monthOverview?.expenditure}
+          layout={'vertical'}
+          status="error"
+          prefix={'¥'}
+          valueStyle={{ color: '#cf1322' }}
+        />
+      </StatisticCard>
+      <Divider />
+      <StatisticCard title="周概览">
+        <Statistic
+          title="收入"
+          precision={2}
+          value={props.value.weekOverview?.income}
+          layout={'vertical'}
+          status={'success'}
+          prefix={'¥'}
+          valueStyle={{ color: '#3f8600' }}
+        />
+        <Statistic
+          title="支出"
+          precision={2}
+          value={props.value.weekOverview?.expenditure}
+          layout={'vertical'}
+          status="error"
+          prefix={'¥'}
+          valueStyle={{ color: '#cf1322' }}
+        />
+      </StatisticCard>
+    </StatisticCard.Group>
+  );
+};
+export default Overview;
--- src/pages/Dashboard/data.d.ts
+++ src/pages/Dashboard/data.d.ts
@@ -1,13 +1,13 @@
-export type ClassifyInfo = {
-  classifyId: string;
-  classifyName: string;
-  classifyType: number;
-  classifyDescribe: string;
-  createTime: Date;
+export type FundsOverview = {
+  overview: Overview;
+  yearOverview: Overview;
+  monthOverview: Overview;
+  weekOverview: Overview;
+  assets: number;
+  liabilities: number;
 };
 
-export type TableListPagination = {
-  totalSize: number;
-  pageSize: number;
-  pageNumber: number;
+export type Overview = {
+  income: number;
+  expenditure: number;
 };
