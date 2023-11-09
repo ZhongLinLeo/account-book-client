621da5d455c242e8d8e2bae4d3acab275f0c4dc9
lin.zl <lin.zl@dtwave-inc.com>
2023-11-09 18:09:02 +0800

    feat(dashboard): add funds record template

--- src/pages/Dashboard/components/TopList.tsx
+++ src/pages/Dashboard/components/TopList.tsx
@@ -1,27 +1,50 @@
 import { Top } from '@/pages/Dashboard/data';
-import { List } from 'antd';
+import { Popover, Table, Typography } from 'antd';
 
 import React from 'react';
 import { ProCard } from '@ant-design/pro-components';
+import type { ColumnsType } from 'antd/es/table';
+
+const { Text } = Typography;
 
 export type TopProps = {
   top: Partial<Top[]>;
+  type: boolean;
 };
 
 const TopsList: React.FC<TopProps> = (props) => {
+  const columns: ColumnsType<Top> = [
+    {
+      title: '金额',
+      dataIndex: 'fundsRecordBalance',
+      key: 'fundsRecordBalance',
+      width: '60%',
+      render: (_, item) => {
+        return (
+          <Text strong type={props.type ? 'danger' : 'success'}>
+            {props.type ? '-' : '+'}
+            {item.fundsRecordBalance}
+          </Text>
+        );
+      },
+    },
+    {
+      title: '时间',
+      dataIndex: 'fundsRecordTime',
+      key: 'fundsRecordTime',
+      render: (_, item) => {
+        return <Text strong>{item.fundsRecordTime}</Text>;
+      },
+    },
+  ];
+
   return (
-    <List
-      itemLayout="horizontal"
+    <Table
+      showHeader={false}
+      columns={columns}
       dataSource={props.top}
-      size="small"
-      renderItem={(item, index) => (
-        <ProCard split="vertical">
-          <ProCard>{index}</ProCard>
-          <ProCard>{item.fundsRecordBalance}</ProCard>
-          <ProCard>{item.fundsRecordDescribe}</ProCard>
-          <ProCard>{item.fundsRecordTime}</ProCard>
-        </ProCard>
-      )}
+      size={'small'}
+      pagination={false}
     />
   );
 };
